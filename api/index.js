7f6e77e7a63af51261b5f0a2ece6bd1483f93451
叶子 <yezi@yezideMacBook-Air.local>
2026-02-10 14:45:42 +0800

    修改配置尝试修复500错误

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const app = require('../main') // 引入根目录的 app.js
+
+// 这一步是为了适配 Vercel 的 Serverless 环境
+module.exports = (req, res) => {
+  // 某些版本的 API 需要手动设置 cookie 保存路径（虽然 Vercel 还是写不了，但能防止崩溃）
+  // 这里其实主要是为了导出一个函数给 Vercel 调用
+  return app(req, res)
+}
